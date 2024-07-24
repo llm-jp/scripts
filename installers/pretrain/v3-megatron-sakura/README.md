0dc647972df8b1aa06b265ee431707aec17b2e69
odashi <yusuke.oda@predicate.jp>
2024-07-24 21:08:32 +0900

    Add v3-megatron-sakura pretrain installer

---
+++ installers/pretrain/v3-megatron-sakura/README.md
@@ -0,0 +1,39 @@
+# Megatron installation script for Sakura/LLM-jp v3 models
+
+LLM-jp v3モデルを作成するためのMegatron環境をSakuraクラスタにインストールするためのスクリプトです。
+
+## Usage
+
+### Prerequisites
+
+pyenv上にPython 3.10.14をインストールしてください。ここから派生させたvenv上に環境を構築します。
+
+```shell
+curl https://pyenv.run | bash
+
+echo 'export PATH="/home/user/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+exec bash
+
+pyenv install 3.10.14
+```
+
+### Build
+
+```shell
+git clone git@github.com:llm-jp/llm-jp-scripts
+cp -a llm-jp-scripts/installers/v3-megatron-sakura ~/myspace
+cd myspace # ワーキングディレクトリ
+
+pyenv local 3.10.14
+which python # Pythonのパスを確認
+python --version # Pythonのバージョンを確認
+
+python -m venv venv
+source venv/bin/activate
+
+bash install.sh
+# ワーキングディレクトリにツール一式がインストールされます。時間がかかるので気長に待って下さい。
+```
---
+++ installers/pretrain/v3-megatron-sakura/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eu -o pipefail
+
+module load cuda/12.1
+module load /data/cudnn-tmp-install/modulefiles/8.9.4
+module load hpcx/2.17.1-gcc-cuda12/hpcx
+module load nccl/2.20.5
+
+# install basic requirements
+pip install -U pip cmake wheel ninja
+pip install -r requirements.txt
+pip install zarr tensorstore
+
+# install apex
+git clone https://github.com/NVIDIA/apex -b 24.04.01
+cd apex/
+MAX_JOBS=32 pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
+cd ../
+
+# install flash attention
+pip install flash-attn==2.4.2 --no-build-isolation
+
+# install transformer engine
+pip install git+https://github.com/NVIDIA/TransformerEngine.git@v1.4
+
+# download our Megatron and build helper library
+git clone git@github.com:llm-jp/Megatron-LM -b nii-geniac
+cd Megatron-LM/megatron/core/datasets/
+make

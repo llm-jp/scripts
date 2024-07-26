6e95472e3457c78ba2d4294b1db7acdc18292978
odashi <yusuke.oda@predicate.jp>
2024-07-27 02:57:23 +0900

    apply suggestions

--- installers/pretrain/v3-megatron-sakura/install.sh
+++ installers/pretrain/v3-megatron-sakura/install.sh
@@ -7,14 +7,20 @@ module load hpcx/2.17.1-gcc-cuda12/hpcx
 module load nccl/2.20.5
 
 # install basic requirements
-pip install -U pip cmake wheel ninja
-pip install -r requirements.txt
-pip install zarr tensorstore
+pip install -U pip==24.1.2
+pip install -U -r requirements.txt
 
 # install apex
 git clone https://github.com/NVIDIA/apex -b 24.04.01
 cd apex/
-MAX_JOBS=32 pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
+MAX_JOBS=32 pip install \
+    -v \
+    --disable-pip-version-check \
+    --no-cache-dir \
+    --no-build-isolation \
+    --config-settings "--build-option=--cpp_ext" \
+    --config-settings "--build-option=--cuda_ext" \
+    ./
 cd ../
 
 # install flash attention
@@ -24,7 +30,7 @@ pip install flash-attn==2.4.2 --no-build-isolation
 pip install git+https://github.com/NVIDIA/TransformerEngine.git@v1.4
 
 # download our Megatron and build helper library
-git clone git@github.com:llm-jp/Megatron-LM -b nii-geniac
+git clone https://github.com:llm-jp/Megatron-LM -b nii-geniac
 cd Megatron-LM/megatron/core/datasets/
 make
 

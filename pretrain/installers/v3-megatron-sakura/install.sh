d9528db689fae1e6cf2dda14fefec23dcd7f7726
odashi <yusuke.oda@predicate.jp>
2024-07-28 18:45:32 +0900

    tempolaly save

--- pretrain/installers/v3-megatron-sakura/install.sh
+++ pretrain/installers/v3-megatron-sakura/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Installation script for LLM-jp Megatron-LM on Sakura cluster
 # Usage: bash install.sh /path/to/myspace
-set -euxo pipefail
+set -eux -o pipefail
 
 TARGET_DIR=$1; shift
 
@@ -31,43 +31,64 @@ if [ "$(python --version)" != "Python $INSTALLER_PYTHON_VERSION" ]; then
 fi
 python -m venv venv
 source venv/bin/activate
+
 python -m pip install -U pip==$INSTALLER_PIP_VERSION
-python -m pip install -U -r installer/requirements.txt
+python -m pip install -U -r installer/requirements_initial.txt
 
 mkdir src
+pushd src
+
+# pytorch
+git clone git@github.com:pytorch/pytorch -b v$PYTORCH_VERSION
+pushd pytorch
+git submodule update --init --recursive
+export PYTORCH_BUILD_VERSION=${PYTORCH_VERSION}+cu${CUDA_VERSION_SHORT}
+export PYTORCH_BUILD_NUMBER=1
+python setup.py install
+popd
+
+# torchvision
+git clone git@github.com:pytorch/vision -b v$TORCHVISION_VERSION
+pushd vision
+python setup.py install
+popd
+
+# install pytorch
+#python -m pip install \
+#    --find-links https://download.pytorch.org/whl/torch_stable.html \
+#    torch==${INSTALLER_PYTORCH_VERSION}+cu${INSTALLER_CUDA_VERSION_SHORT}
+#python -m pip install \
+#    --find-links https://download.pytorch.org/whl/torch_stable.html \
+#    torchvision==${INSTALLER_TORCHVISION_VERSION}+cu${INSTALLER_CUDA_VERSION_SHORT}
+
+popd  # src
+
+# install other requirements
+python -m pip install -U -r installer/requirements_final.txt
+
 pushd src
 
 # install apex
 git clone https://github.com/NVIDIA/apex -b $INSTALLER_APEX_VERSION
 pushd apex
-MAX_JOBS=32 pip install \
-    -v \
-    --disable-pip-version-check \
+MAX_JOBS=32 python -m pip install \
     --no-build-isolation \
-    --no-cache-dir \
     --config-settings "--build-option=--cpp_ext" \
     --config-settings "--build-option=--cuda_ext" \
     .
 popd
 
-# install flash attention
-git clone https://github.com/Dao-AILab/flash-attention -b v$INSTALLER_FLASH_ATTENTION_VERSION
-pushd flash-attention
-pip install \
-    -v \
-    --disable-pip-version-check \
-    --no-build-isolation \
-    --no-cache-dir \
-    .
-popd
-
 # install transformer engine
-
-pip install \
-    -v \
-    --disable-pip-version-check \
+MAX_JOBS=32 python -m pip install \
     --no-build-isolation \
     git+https://github.com/NVIDIA/TransformerEngine.git@v$INSTALLER_TRANSFORMER_ENGINE_VERSION
+
+# reinstall flash attention
+#python -m pip uninstall -y flash-attn
+#git clone https://github.com/Dao-AILab/flash-attention -b v$INSTALLER_FLASH_ATTENTION_VERSION
+#pushd flash-attention
+#MAX_JOBS=32 pip install --no-build-isolation -e .
+#popd
 
 # download our Megatron and build helper library
 git clone https://github.com/llm-jp/Megatron-LM -b $INSTALLER_MEGATRON_TAG

2970445fbda587e621c7862e3b4e1a57c49f7c68
Kouta Nakayama <kouta.nakayama@gmail.com>
2024-08-02 18:29:49 +0900

    Add script to check file size and md5sum of checkpoint on Google Cloud Storage (#7)
    Co-authored-by: k141303 <nakayama@nii.ac.jp>

---
+++ pretrain/scripts/v3-megatron-gcp/calculate_gcloud_combined_md5.sh
@@ -0,0 +1,42 @@
+
+#!/bin/bash
+
+# This script calculates the combined md5sum of all files in a given Google Cloud Storage bucket/directory.
+# The combined md5sum is calculated by fetching the md5sums of each file and then combining them.
+#
+# How to use:
+# bash calculate_gcloud_combined_md5.sh $GOOGLE_CLOUD_DIR
+# > xxxxxxxxxxx(md5sum)
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <gcs-directory>"
+  exit 1
+fi
+
+GCS_DIRECTORY=$1
+
+# Create a temporary file to store individual md5sums
+temp_file=$(mktemp)
+
+# Function to convert base64 md5 to hex
+base64_to_hex() {
+  echo "$1" | base64 --decode | od -An -t x1 | tr -d ' \n'
+}
+
+# List all files in the GCS directory and fetch their md5sums
+gsutil ls -r "$GCS_DIRECTORY/**" | grep -v '/$' | while read -r file; do
+  md5_base64=$(gsutil stat "$file" | grep "Hash (md5)" | awk '{print $3}')
+  if [ -n "$md5_base64" ]; then
+    md5_hex=$(base64_to_hex "$md5_base64")
+    echo "$md5_hex"
+  fi
+done > "$temp_file"
+
+# Sort the md5 sums and calculate the combined md5sum
+combined_md5=$(sort "$temp_file" | md5sum | awk '{print $1}')
+
+# Clean up the temporary file
+rm "$temp_file"
+
+echo "$combined_md5"
+
--- pretrain/scripts/v3-megatron-gcp/check_cloud_checkpoints.sh
+++ pretrain/scripts/v3-megatron-gcp/check_cloud_checkpoints.sh
@@ -4,12 +4,15 @@
 # If the file sizes are different, an error is output.
 # 
 # How to use:
-# $ bash check_cloud_filesize.sh
+# $ bash check_cloud_checkpoints.sh
 # > iter_xxxx Local size:yyyy Cloud size:yyyy
+# > iter_xxx Local md5sum:hogehoge Cloud md5sum:hogehoge
 # If the file sizes are different
 # > iter_xxxx Local size:yyyy Cloud size:zzzz
 # > Error: iter_xxxx file sizes are different.
- 
+# > iter_xxxx Local md5sum:hogehoge Cloud md5sum:hugehuge
+# > Error: iter_xxx md5 checksums are different.
+
 LOCAL_CKPT_DIR="/lustre/checkpoints/llama-2-172b-exp2/tp4-pp16-cp1" # FIXME: DIR_PATH to checkpoint on local storage
 CLOUD_CKPT_DIR="gs://llama2-172b-checkpoint-exp2" # FIXME: DIR_PATH to checkpoint on Google Cloud Storage
 
@@ -22,5 +25,15 @@ for iter in $(ls ${LOCAL_CKPT_DIR} | grep iter_); do
 	if [ "$local_size" -ne "$cloud_size" ]; then
 		echo "Error: ${iter} file sizes are different."
 	fi
+
+	local_combine_md5=$(bash calculate_combined_md5.sh ${LOCAL_CKPT_DIR}/${iter})
+	cloud_combine_md5=$(bash calculate_gcloud_combined_md5.sh ${CLOUD_CKPT_DIR}/${iter})
+
+	echo "${iter} Local md5sum:$local_combine_md5 Cloud md5sum:$cloud_combine_md5"
+
+	if [ "$local_combine_md5" != "$cloud_combine_md5" ]; then
+		echo "Error: ${iter} md5 checksums are different."
+	fi
+
 done
 

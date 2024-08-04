eb3d7a05407a96a83f118804902a4fcd53e79550
Hirokazu Kiyomaru <13678589+hkiyomaru@users.noreply.github.com>
2024-08-04 13:00:48 +0900

    Add a script to submit models for the GENIAC official evaluation (#8)

---
+++ pretrain/geniac-evaluation/submit.py
@@ -0,0 +1,47 @@
+"""Submit a model as a W&B artifact."""
+import wandb
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument(
+    "--entity",
+    type=str,
+    required=True,
+    help="The W&B entity to which the model will be uploaded.",
+)
+parser.add_argument(
+    "--project",
+    type=str,
+    required=True,
+    help="The W&B project to which the model will be uploaded.",
+)
+parser.add_argument(
+    "--model-path",
+    type=str,
+    required=True,
+    help="The path to the model directory to be uploaded.",
+)
+parser.add_argument(
+    "--model-name",
+    type=str,
+    required=True,
+    help="The name of the model to be uploaded.",
+)
+parser.add_argument(
+    "-v",
+    "--model-version",
+    type=str,
+    required=True,
+    help="The version of the model to be uploaded.",
+)
+args = parser.parse_args()
+
+with wandb.init(entity=args.entity, project=args.project, job_type="upload_data") as run:
+    model_artifact = wandb.Artifact(
+        name=args.model_name,
+        type="model",
+        metadata={"version": args.model_version},
+        description=f"{args.model_name} (version: {args.model_version})",
+    )
+    model_artifact.add_dir(args.model_path, skip_cache=True, policy="immutable")
+    run.log_artifact(model_artifact)

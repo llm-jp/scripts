601c1935321a1c66911796171244639d2e56a3c8
Yusuke Oda <yusuke.oda@predicate.jp>
2024-08-14 16:32:23 +0900

    Specify number of CPUs/RAM for installer jobs (#16)

--- pretrain/installers/v3-megatron-sakura/install.sh
+++ pretrain/installers/v3-megatron-sakura/install.sh
@@ -14,8 +14,10 @@
 
 #SBATCH --job-name=pretrain-install
 #SBATCH --partition=cpu
+#SBATCH --exclusive
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
+#SBATCH --mem=0
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 

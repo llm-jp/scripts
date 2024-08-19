d38225e0ec9b790823f53149ef1172a3603b0c0d
Yuma Tsuta <yuma-tsuta@nii.ac.jp>
2024-08-19 18:43:26 +0900

    Add guide of function

--- common/installers.sh
+++ common/installers.sh
@@ -1,5 +1,10 @@
 install_python() {
-  # Install Python
+  # This function installs CPython.
+  # Usage: install_python BRANCH_TAG TARGET_DIR
+  # Arguments:
+  #  - BRANCH_TAG: Branch or tag of CPython to be checked out.
+  #  - TARGET_DIR: Directory where CPython will be installed. 
+
   if [ $# -ne 2 ]; then
     set +x
     >&2 echo Usage: install_python BRANCH_TAG TARGET_DIR

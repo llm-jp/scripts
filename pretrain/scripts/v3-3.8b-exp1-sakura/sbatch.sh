7a4157a9d2e9f6e2f1480c0f1ad58e9f13e8cd65
odashi <yusuke.oda@predicate.jp>
2024-08-21 20:47:00 +0900

    fix path

--- pretrain/scripts/v3-3.8b-exp1-sakura/sbatch.sh
+++ pretrain/scripts/v3-3.8b-exp1-sakura/sbatch.sh
@@ -38,4 +38,4 @@ mpirun \
   -x MASTER_PORT=$MASTER_PORT \
   -x NUM_NODES=$NUM_NODES \
   -x NUM_GPUS_PER_NODE=$NUM_GPUS_PER_NODE \
-  bash scripts/pretrain/scripts/v3-3.8m-exp1-sakura/train.sh
+  bash scripts/pretrain/scripts/v3-3.8b-exp1-sakura/train.sh

3a7d7a2d208f5bcaa75b101da844346e3da189c4
Yuma Tsuta <yuma_tsuta@nii.ac.jp>
2024-09-24 17:22:03 +0900

    Fix to use setup.sh

--- server/mdx/lustre_setup/no_arg_setup.sh
+++ server/mdx/lustre_setup/no_arg_setup.sh
@@ -8,51 +8,6 @@
 #
 set -eux -o pipefail
 
-
-USAGE_MSG="Usage: sudo bash no_arg_setup.sh"
-
-# Check that no arguments are passed to the script
-if [ $# -ne 0 ]; then
-  echo "$USAGE_MSG"
-  exit 1
-fi
-
-# Check if running on a login node
-if [[ "$(uname -n)" == "login-node" ]]; then
-  echo >&2 "This script cannot be run on a login node."
-  exit 1
-fi
-
-# Ensure the script is being run as root (via sudo)
-if [ "$(id -u)" -ne 0 ]; then
-  echo >&2 "This script must be run as root. Use sudo."
-  exit 1
-fi
-
-# Network configuration setup
-cp /etc/lnet.conf.ddn.j2 /etc/lnet.conf.ddn
-
-# Get the network name from the third line of `ip -br addr`
-NETWORK_NAME=$(ip -br addr | sed -n 3p | awk '{print $1}')
+# Get the STORAGE_NETWORK1_IPV4 from the third line of `ip -br addr`
 STORAGE_NETWORK1_IPV4=$(ip -br addr | sed -n 3p | awk '{print $3}' | cut -d'/' -f1)
-
-# Error checking for network name detection
-if [ -z "$NETWORK_NAME" ] || [ -z "$STORAGE_NETWORK1_IPV4" ]; then
-  echo >&2 "Error: Network interface or IP address could not be detected."
-  exit 1
-fi
-
-# Update lnet configuration file with detected network interface and IP address
-sed -i "s/{{ tcp_src_ipaddr }}/$STORAGE_NETWORK1_IPV4/g" /etc/lnet.conf.ddn
-sed -i "s/{{ ib_src_ipaddr }}/$STORAGE_NETWORK1_IPV4/g" /etc/lnet.conf.ddn
-sed -i "s/{{ ib_netif }}/$NETWORK_NAME/g" /etc/lnet.conf.ddn
-sed -i "s/{{ tcp_netif }}/$NETWORK_NAME/g" /etc/lnet.conf.ddn
-
-# Modify /etc/fstab to uncomment relevant Lustre entries
-sed -i '/^#172\.17\.8\./s/^#//' /etc/fstab
-
-# Enable Lustre client service
-systemctl enable lustre_client
-
-# Reboot the system to apply the changes
-reboot
+bash setup.sh -f $STORAGE_NETWORK1_IPV4

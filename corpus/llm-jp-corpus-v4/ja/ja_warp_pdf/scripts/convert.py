75f5a2ff90de1443a6b0c67dbdec3d2e972af419
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2025-01-16 12:44:10 +0900

    tweak

--- corpus/llm-jp-corpus-v4/ja/ja_warp_pdf/scripts/convert.py
+++ corpus/llm-jp-corpus-v4/ja/ja_warp_pdf/scripts/convert.py
@@ -144,6 +144,5 @@ def main() -> None:
 if __name__ == "__main__":
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
-        level=logging.INFO,
     )
     main()

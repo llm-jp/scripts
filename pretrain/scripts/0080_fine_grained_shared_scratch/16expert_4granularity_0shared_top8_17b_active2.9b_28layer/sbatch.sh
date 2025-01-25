8451f139d494e4ee9843510804b9ca011b8be57e
Taishi-N324 <taishiahiru5@gmail.com>
2025-01-26 03:31:11 +0900

    Add 16expert_4granularity_0shared_top8_17b_active2.9b_28layer 16expert_4granularity_0shared_top8_17b_active2.9b_32layer 16expert_4granularity_0shared_top8_17b_active2.9b_z_loss_from_150k 16expert_4granularity_2shared_top6_18b_active2.9b 32expert_4granularity_0shared_top8_34b_active2.9b 8expert_1granularity_0shared_top4_9.2b_active5.0b_pre-softmax 8expert_1granularity_0shared_top8_9.2b_active9.2b

---
+++ pretrain/scripts/0080_fine_grained_shared_scratch/16expert_4granularity_0shared_top8_17b_active2.9b_28layer/sbatch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=0080
+#SBATCH --partition=gpu-small
+#SBATCH --nodes=4
+#SBATCH --gpus-per-node=8
+#SBATCH --ntasks-per-node=8
+#SBATCH --output=outputs/%x-%j.out
+#SBATCH --error=outputs/%x-%j.err
+
+set -eu -o pipefail
+
+source scripts/environment.sh
+source venv/bin/activate
+
+export MASTER_ADDR=$(scontrol show hostname $SLURM_JOB_NODELIST | head -n1)
+export MASTER_PORT=$((10000 + ($SLURM_JOBID % 50000)))
+
+echo "MASTER_ADDR=${MASTER_ADDR}"
+
+NUM_NODES=$SLURM_JOB_NUM_NODES
+NUM_GPUS_PER_NODE=$(echo $SLURM_TASKS_PER_NODE | cut -d '(' -f 1)
+NUM_GPUS=$((${NUM_NODES} * ${NUM_GPUS_PER_NODE}))
+
+echo NUM_NODES=$NUM_NODES
+echo NUM_GPUS_PER_NODE=$NUM_GPUS_PER_NODE
+echo NUM_GPUS=$NUM_GPUS
+
+mpirun \
+  -np $NUM_GPUS \
+  --npernode $NUM_GPUS_PER_NODE \
+  -bind-to none \
+  -map-by slot \
+  -x MASTER_ADDR=$MASTER_ADDR \
+  -x MASTER_PORT=$MASTER_PORT \
+  -x NUM_NODES=$NUM_NODES \
+  -x NUM_GPUS_PER_NODE=$NUM_GPUS_PER_NODE \
+  bash scripts/16expert_4granularity_0shared_top8_17b_active2.9b_28layer/train.sh

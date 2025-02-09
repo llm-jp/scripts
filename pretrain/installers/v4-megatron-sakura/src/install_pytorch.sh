54ab28355c7107ed03f0cd0bf426e04191f9d1cf
odashi <yusuke.oda@predicate.jp>
2025-02-09 10:09:45 +0900

    fix pytorch installation

--- pretrain/installers/v4-megatron-sakura/src/install_pytorch.sh
+++ pretrain/installers/v4-megatron-sakura/src/install_pytorch.sh
@@ -6,9 +6,9 @@ source ${TARGET_DIR}/venv/bin/activate
 
 python -m pip install \
   --no-cache-dir \
-  --index-url https://download.pytorch.org/whl/cu${PRETRAIN_CUDA_VERSION_SHORT} \
   --find-links https://download.pytorch.org/whl/torch_stable.html \
-  torch==${PRETRAIN_TORCH_VERSION} \
-  torchvision==${PRETRAIN_TORCHVISION_VERSION}
+  torch==${PRETRAIN_TORCH_VERSION}+cu${PRETRAIN_CUDA_VERSION_SHORT} \
+  torchvision==${PRETRAIN_TORCHVISION_VERSION}+cu${PRETRAIN_CUDA_VERSION_SHORT}
+
 
 deactivate

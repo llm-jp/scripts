87fe234d01b2eb4ebaa64b80486d8783c02cdbd4
odashi <yusuke.oda@predicate.jp>
2025-02-14 19:49:32 +0900

    add 1.3b

---
+++ pretrain/scripts/v4-training/params/1.3b-llama3-ecjk.sh
@@ -0,0 +1,123 @@
+# Pretraining hyperparameters for v4 1.3B.
+# Model card: https://github.com/llm-jp/model-cards/pull/31
+
+ALL_PARAMS=()
+
+# Model hyperparameters
+ALL_PARAMS+=(
+    --num-layers 16
+    --hidden-size 2048
+    --ffn-hidden-size 7168
+    --num-attention-heads 16
+    --group-query-attention
+    --num-query-groups 8
+    --seq-length 8192
+    --max-position-embeddings 8192
+    --position-embedding-type rope
+    --rotary-base 500000
+    --untie-embeddings-and-output-weights
+    --swiglu
+    --normalization RMSNorm
+    --norm-epsilon 1e-5
+    --disable-bias-linear
+)
+
+# Tokenizer
+ALL_PARAMS+=(
+    --tokenizer-type Llama2Tokenizer
+    --tokenizer-model ${ENV_DIR}/src/llm-jp-tokenizer/models/ver3.0/llm-jp-tokenizer-100k.ver3.0b1.model
+)
+
+# Optimizer hyperparameters
+ALL_PARAMS+=(
+    --optimizer adam
+    --lr 3e-4
+    --min-lr 3e-5
+    --adam-beta1 0.9
+    --adam-beta2 0.95
+    --adam-eps 1e-8
+    --clip-grad 1.0
+    --weight-decay 0.1
+    --init-method-std 0.02
+    --attention-dropout 0.0
+    --hidden-dropout 0.0
+)
+
+# ceil( 15.6T / 8192 / 1024 ) == 1859665
+TRAIN_ITERS=1859665
+
+# Scheduler
+ALL_PARAMS+=(
+    --train-iters ${TRAIN_ITERS}
+    --lr-warmup-iters 2000
+    --lr-decay-iters ${TRAIN_ITERS}
+    --lr-decay-style cosine
+    --eval-interval 999999999
+    --eval-iters 0
+)
+
+# Batch sizes
+ALL_PARAMS+=(
+    --micro-batch-size 1
+    --global-batch-size 1024
+)
+
+# Parallelism
+ALL_PARAMS+=(
+    --tensor-model-parallel-size 1
+    --pipeline-model-parallel-size 1
+    --context-parallel-size 1
+    --sequence-parallel
+    --use-distributed-optimizer
+    --distributed-backend nccl
+    # NOTE(odashi): Increasing timeout is required to prepare 15.6T dataset.
+    --distributed-timeout-minutes 120
+    --use-mpi
+)
+
+# Dataset
+ALL_PARAMS+=(
+    --data-cache-path ${MODEL_DIR}/cache
+    --split 1,0,0
+)
+
+TASK_CHECKPOINT_DIR=${MODEL_DIR}/checkpoints
+
+# Checkpointing
+ALL_PARAMS+=(
+    --load ${TASK_CHECKPOINT_DIR}
+    --save ${TASK_CHECKPOINT_DIR}
+    --save-interval 1000
+)
+
+# Other implementation-related parameters
+ALL_PARAMS+=(
+    --bf16
+    --use-mcore-models
+    --no-masked-softmax-fusion
+    --use-flash-attn
+
+    # NOTE(odashi): For adjusting throughput
+    #--recompute-activations
+    #--recompute-granularity selective
+    #--overlap-grad-reduce
+    #--overlap-param-gather
+
+    --attention-softmax-in-fp32
+    --transformer-impl transformer_engine
+
+    # NOTE(odashi): Newer implementation requires to set attention backend by parameter.
+    #--attention-backend flash
+)
+
+# NOTE(odashi): Disable fused attention for Sakura cluster due to some inconsistency.
+export NVTE_FUSED_ATTN=0
+
+# Logging
+ALL_PARAMS+=(
+    --log-interval 1
+    --log-throughput
+    --wandb-entity ${WANDB_ENTITY}
+    --wandb-project ${WANDB_PROJECT}
+    --wandb-exp-name train_$(date '+%Y%m%d-%H%M%S')_${SLURM_JOB_ID}
+)
--- pretrain/scripts/v4-training/sbatch_train.sh
+++ pretrain/scripts/v4-training/sbatch_train.sh
@@ -11,26 +11,29 @@
 #     --job-name=9999_train \
 #     --partition=gpu \
 #     --nodes=64 \
-#     --outpout=train.slurm.out \
+#     --outpout=train_%j.log \
 #     sbatch_train.sh \
-#       /path/to/env \
-#       /path/to/model \
-#       llm-jp \
-#       9999_train
+#       /path/to/env \     ... ENV_DIR: path to the trainer environment
+#       /path/to/model \   ... MODEL_DIR: path to the model to save
+#       7.7b-llama3-ecjk \ ... PARAM_NAME: model config; corresponding file in `params/` should exist
+#       llm-jp \           ... WANDB_ENTITY
+#       9999_train         ... WANDB_PROJECT
 
 set -eu -o pipefail
 
 # Arguments
-if [ $# -ne 4 ]; then
-    >&2 echo "Usage: $0 ENV_DIR MODEL_DIR WANDB_ENTITY WANDB_PROJECT"
+if [ $# -ne 5 ]; then
+    >&2 echo "Usage: $0 ENV_DIR MODEL_DIR PARAM_NAME WANDB_ENTITY WANDB_PROJECT"
     exit 1
 fi
 ENV_DIR=$(realpath -eP $1); shift
 MODEL_DIR=$(realpath -m $1); shift
+PARAM_NAME=$1; shift
 WANDB_ENTITY=$1; shift
 WANDB_PROJECT=$1; shift
 echo "ENV_DIR=${ENV_DIR}"
 echo "MODEL_DIR=${MODEL_DIR}"
+echo "PARAM_NAME=${PARAM_NAME}"
 
 # Find the script directory
 if [ -n "${SLURM_JOB_ID:-}" ]; then
@@ -46,15 +49,13 @@ SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
 echo "SCRIPT_DIR=${SCRIPT_DIR}"
 
 # Setup environment
-source ${SCRIPT_DIR}/setup.sh
+source ${SCRIPT_DIR}/common/setup.sh
 
 # Load ALL_PARAMS
-source ${SCRIPT_DIR}/params.sh
+source ${SCRIPT_DIR}/params/${PARAM_NAME}.sh
 
 # Load TRAIN_DATA_PATH
-source ${SCRIPT_DIR}/../train_data/llama3_simulation_15_6t.sh
-# NOTE(odashi): For testing on small corpus
-#source ${SCRIPT_DIR}/../train_data/small.sh
+source ${SCRIPT_DIR}/train_data/llama3_simulation_15_6t.sh
 
 # Run the trainer script
 mpirun \

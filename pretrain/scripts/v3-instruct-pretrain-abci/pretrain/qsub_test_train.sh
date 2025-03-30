c78fca3cf83652306952286c69e95656d9dcc84d
Taka008 <tkodama@nii.ac.jp>
2025-03-30 20:00:14 +0900

    use torch 2.5.1 for test scripts

--- pretrain/scripts/v3-instruct-pretrain-abci/pretrain/qsub_test_train.sh
+++ pretrain/scripts/v3-instruct-pretrain-abci/pretrain/qsub_test_train.sh
@@ -10,18 +10,18 @@ cd $PBS_O_WORKDIR
 
 JOBID=${PBS_JOBID%%.*}
 mkdir -p ${TASK_DIR}/logs
-LOGFILE=${TASK_DIR}/logs/train-${HOST_NAME}-${JOBID}.out
-ERRFILE=${TASK_DIR}/logs/train-${HOST_NAME}-${JOBID}.err
+LOGFILE=${TASK_DIR}/logs/${HOST_NAME}-${JOBID}.out
+ERRFILE=${TASK_DIR}/logs/${HOST_NAME}-${JOBID}.err
 exec > $LOGFILE 2> $ERRFILE
 
 set -eu -o pipefail
 
 EXPERIMENT_DIR=/groups/gcg51557/experiments/0130_instruction_pretraining
 SCRIPT_DIR=/groups/gcg51557/experiments/0130_instruction_pretraining/scripts/pretrain/scripts/v3-instruct-pretrain-abci/pretrain
-ENV_DIR=${EXPERIMENT_DIR}/environments/pretrain_torch_v2.6.0
+ENV_DIR=${EXPERIMENT_DIR}/environments/pretrain_torch_v2.5.1
 
 # Setup environment
-source ${SCRIPT_DIR}/common/setup.sh
+source ${SCRIPT_DIR}/common/setup_torch_v2.5.1.sh
 
 export MASTER_ADDR=$(head -n 1 $PBS_NODEFILE | hostname -f)
 export MASTER_PORT=$((10000 + RANDOM % 1000))
@@ -40,8 +40,6 @@ cat $PBS_NODEFILE
 # Load ALL_PARAMS
 source ${SCRIPT_DIR}/params/${PARAM_NAME}.sh
 echo "ALL_PARAMS: ${ALL_PARAMS[@]}"
-
-export NVTE_FUSED_ATTN=0
 
 mpirun \
   --display-allocation \

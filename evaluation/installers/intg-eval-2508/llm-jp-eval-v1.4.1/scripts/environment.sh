f2ecc2fb4565070ecfd4dcb89131ef7211ba3294
Kouta Nakayama <kouta.nakayama@gmail.com>
2025-07-27 20:38:16 +0900

    remove REPOSITORY_URL

--- evaluation/installers/intg-eval-2508/llm-jp-eval-v1.4.1/scripts/environment.sh
+++ evaluation/installers/intg-eval-2508/llm-jp-eval-v1.4.1/scripts/environment.sh
@@ -4,4 +4,3 @@ export TORCH_VERSION=2.3.1
 export TORCHVISION_VERSION=0.18.1
 export LLM_JP_EVAL_TAG=v1.4.1-custom
 export LLM_JP_EVAL_BUG_FIX_COMMIT_IDS=
-export REPOSITORY_URL=https://github.com/llm-jp/jp-eval-customization.git

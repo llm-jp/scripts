5f69da6c932104599022848d65111c98d5b3cd7e
Ryo Takahashi <reiyw.setuve+github@gmail.com>
2025-08-25 18:08:28 +0900

    Update vllm and torch (#94)

--- evaluation/installers/intg-eval-2508/llm-jp-eval-v1.4.1/scripts/environment.sh
+++ evaluation/installers/intg-eval-2508/llm-jp-eval-v1.4.1/scripts/environment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PYTHON_VERSION=3.10.14
-export TORCH_VERSION=2.3.1
+export TORCH_VERSION=2.5.1
 export TORCHVISION_VERSION=0.18.1
 export LLM_JP_EVAL_TAG=1.4.1-custom
 export LLM_JP_EVAL_BUG_FIX_COMMIT_IDS=

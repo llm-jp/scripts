e469ace38a8b88b1640d315a0053c0e61a5acced
Ryo Takahashi <reiyw.setuve@gmail.com>
2025-08-28 14:51:37 +0900

    wip

--- common/installers.sh
+++ common/installers.sh
@@ -1,3 +1,24 @@
+# install_python() {
+#   # This function installs CPython.
+#   # Usage: install_python BRANCH_TAG TARGET_DIR
+#   # Arguments:
+#   #  - BRANCH_TAG: Branch or tag of CPython to be checked out.
+#   #  - TARGET_DIR: Directory where CPython will be installed. 
+
+#   if [ $# -ne 2 ]; then
+#     set +x
+#     >&2 echo Usage: install_python BRANCH_TAG TARGET_DIR
+#     exit 1
+#   fi
+
+#   git clone https://github.com/python/cpython -b $1
+#   pushd cpython
+#   ./configure --prefix=$2 --enable-optimizations
+#   make -j 64
+#   make install
+#   popd 
+# }
+
 install_python() {
   # This function installs CPython.
   # Usage: install_python BRANCH_TAG TARGET_DIR
@@ -11,10 +32,9 @@ install_python() {
     exit 1
   fi
 
-  git clone https://github.com/python/cpython -b $1
-  pushd cpython
-  ./configure --prefix=$2 --enable-optimizations
-  make -j 64
-  make install
-  popd 
+  local temp_dir=$(mktemp -d)
+  uv python install $1 --install-dir $temp_dir
+  local installed_py_dir=$(find $temp_dir -maxdepth 1 -type d -name "cpython-*")
+  mkdir $2
+  cp -r $installed_py_dir/* $2/
 }

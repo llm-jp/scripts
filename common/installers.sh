752d16b54f60a95dc842807871ec7bae69bae779
Ryo Takahashi <reiyw.setuve@gmail.com>
2025-09-17 09:51:20 +0900

    Revert installers.sh

--- common/installers.sh
+++ common/installers.sh
@@ -18,17 +18,3 @@ install_python() {
   make install
   popd 
 }
-
-install_python() {
-  # This function installs CPython.
-  # Usage: install_python BRANCH_TAG TARGET_DIR
-  # Arguments:
-  #  - BRANCH_TAG: Branch or tag of CPython to be checked out.
-  #  - TARGET_DIR: Directory where CPython will be installed. 
-
-  if [ $# -ne 2 ]; then
-    set +x
-    >&2 echo Usage: install_python BRANCH_TAG TARGET_DIR
-    exit 1
-  fi
-}

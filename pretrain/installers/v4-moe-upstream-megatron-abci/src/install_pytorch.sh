0a8cd2a7659bda49d4f5f52eea7397e0aa35e725
Taishi-N324 <taishiahiru5@gmail.com>
2025-11-19 08:50:29 +0900

    fix

--- pretrain/installers/v4-moe-upstream-megatron-abci/src/install_pytorch.sh
+++ pretrain/installers/v4-moe-upstream-megatron-abci/src/install_pytorch.sh
@@ -1,6 +1,6 @@
 # Install pytorch and torchvision
 
-echo "Installing torch ${PRETRAIN_TORCH_VERSION}+cu${PRETRAIN_CUDA_VERSION_SHORT} and torchvision ${PRETRAIN_TORCHVISION_VERSION}+cu${PRETRAIN_CUDA_VERSION_SHORT}"
+echo "Installing torch ${PRETRAIN_TORCH_VERSION}+cu${PRETRAIN_CUDA_VERSION_SHORT}"
 
 source ${TARGET_DIR}/venv/bin/activate
 

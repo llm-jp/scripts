0a703870cc26e6a287317dc16fdd3befa95a9b80
Yusuke Oda <yusuke.oda@predicate.jp>
2026-07-01 14:19:08 +0900

    fix MoE installer (#112)

--- pretrain/installers/v4-moe-upstream-megatron-abci/src/install_flash_attention_3.sh
+++ pretrain/installers/v4-moe-upstream-megatron-abci/src/install_flash_attention_3.sh
@@ -1,29 +1,23 @@
 # Installs flash attention 3 (flash attention for NVIDIA Hopper architecture).
 
-# CAUTION(sosuke):
-# Installing flash attention v2 and v3 in the same environment may cause problems when used with Megatron-LM.
-# We highly recommend only to use flash attention v3 for Hopper architecture.
-
 echo "Installing Flash Attention ${PRETRAIN_FLASH_ATTENTION_VERSION}"
 source ${TARGET_DIR}/venv/bin/activate
 
 pushd ${TARGET_DIR}/src
 
-git clone https://github.com/Dao-AILab/flash-attention.git
-pushd flash-attention/
-git checkout ${PRETRAIN_FLASH_ATTENTION_VERSION}
+git clone https://github.com/Dao-AILab/flash-attention.git -b v${PRETRAIN_FLASH_ATTENTION_VERSION} --recursive
 
-# Use flash-attention 3
-pushd hopper/
-
+pushd flash-attention
+# install v2
 python setup.py install
+pushd hopper
+# install v3
+python setup.py install
+python_path=$(python -c "import site; print(site.getsitepackages()[0])")
+cp ./flash_attn_interface.py ${python_path}/flash_attn_3
+popd
+popd
 
-python_path=`python -c "import site; print(site.getsitepackages()[0])"`
-mkdir -p $python_path/flash_attn_3
-wget -P $python_path/flash_attn_3 https://raw.githubusercontent.com/Dao-AILab/flash-attention/${PRETRAIN_FLASH_ATTENTION_VERSION}/hopper/flash_attn_interface.py
-
-popd # hopper/
-popd # flash-attention/
 popd # ${TARGET_DIR}/src
 
 deactivate
